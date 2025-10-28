fa3069edcd6a6934b63a2d16a33a8b00d582caa2
Kitsune7X <ryuizanagi@proton.me>
2025-10-29 07:28:49 +0900

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,59 @@
+// -------------------------------
+// Variable Declaration
+// -------------------------------
+// Get all the buttons
 const buttons = Array.from(document.querySelectorAll("button"));
 
-console.log(buttons);
+// Get the display
+const displayBottom = document.querySelector("#calculator-display-row-bottom");
+console.log(displayBottom);
 
+// Create an array of number key
+const numberKey = buttons.filter((button) => button.className === "number");
+// console.log(numberKey);
+
+// Create an array of operator key
+const operatorKey = buttons.filter((button) => button.className === "operator");
+console.log(operatorKey);
+
+// -------------------------------
+// Main
+// -------------------------------
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.target.id);
+    // const key = e.target;
+    // console.log(e.target.className);
+
     audio();
-    if (e.target.id === "btn-sound-switch") {
-      mute(e.target);
+    if (key.id === "btn-sound-switch") {
+      mute(key);
+    }
+
+    // When there is no input, display the initial value on the bottom row
+    if (key.className === "number") {
+      // Add comma for each 1000
+      if (Number(displayBottom.textContent) >= 1000) {
+        displayBottom.textContent;
+      }
+      displayBottom.textContent = displayBottom.textContent + key.textContent;
     }
   });
 });
 
+// Display what's being pressed
+
+// -------------------------------
+// Helper functions
+// -------------------------------
+
+// ---------- Play audio function ----------
 function audio() {
   const audio = document.querySelector("#btn-audio");
   audio.currentTime = 0;
   audio.play();
 }
 
+// ---------- Mute audio function ----------
 function mute(target) {
   const audio = document.querySelector("#btn-audio");
   if (audio.muted) {

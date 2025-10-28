71dfb1fcd76665e79908130b7327367167ef2ae3
Kitsune7X <ryuizanagi@proton.me>
2025-10-29 00:48:57 +0900

    feat: complete styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
-console.log("Here!");
+const buttons = Array.from(document.querySelectorAll("button"));
+
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    audio();
+    if (e.target.id === "btn-sound-switch") {
+      mute(e.target);
+      //   e.target.
+    }
+  });
+});
+
+function audio() {
+  const audio = document.querySelector("#btn-audio");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function mute(target) {
+  const audio = document.querySelector("#btn-audio");
+  if (audio.muted) {
+    audio.muted = false;
+    target.textContent = "ON";
+  } else {
+    audio.muted = true;
+    target.textContent = "OFF";
+  }
+}
